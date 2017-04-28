159e932dc26c8ffd48ae1d4d44811b8d2364669a
Javier Torrez <javierctorrez@gmail.com>
2017-04-27 22:35:05 -0400

    added LinkedList

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class ListNode {
+	int item;
+	ListNode next; 
+	
+	public ListNode(int item, ListNode next){
+		this.item = item;
+		this.next = next; 
+	}
+	
+	public ListNode(int item){
+		this(item, null); 
+	}
+	
+	public static void main(String[] args){
+		ListNode l1 = new ListNode(4, new ListNode(7, 
+								      new ListNode(6)));
+		
+
+		
+		// Creating a reference (pointer) to the respective
+		// node assigned to each ListNode
+		
+		/*	Node Operations */
+		
+		
+	
+	}
+}
